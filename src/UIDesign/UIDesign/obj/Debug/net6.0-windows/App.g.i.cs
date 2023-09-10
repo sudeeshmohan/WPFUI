c4915cc2a811677da081785095d295c36c19080d
Sudeesh mohan <sudeeshkakkakattu@gmail.com>
2023-09-10 11:43:46 +0530

    UI close btn

--- src/UIDesign/UIDesign/obj/Debug/net6.0-windows/App.g.i.cs
+++ src/UIDesign/UIDesign/obj/Debug/net6.0-windows/App.g.i.cs
@@ -47,7 +47,7 @@ namespace UIDesign {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.6.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.7.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -73,7 +73,7 @@ namespace UIDesign {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.6.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.7.0")]
         public static void Main() {
             UIDesign.App app = new UIDesign.App();
             app.InitializeComponent();
